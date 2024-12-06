286ad285071b3432af3c10324e2e1f6eaeec2dbf
jamaisseule <nganttqgcd201774@fpt.edu.vn>
2024-12-06 15:16:23 +0700

    feat(VIPER-59): fix ui product items and product detail

--- src/components/product-details/product-image.tsx
+++ src/components/product-details/product-image.tsx
@@ -1,23 +1,30 @@
 import { Image } from 'antd';
+
 type ProductImagesProps = {
   images: string[]; // Danh sách ảnh nhỏ
   onImageSelect: (image: string) => void; // Hàm gọi khi chọn ảnh
+  selectedImage: string | null; // Trạng thái ảnh đã chọn
 };
 
-const ProductImages = ({ images, onImageSelect }: ProductImagesProps) => {
+const ProductImages = ({ images, onImageSelect, selectedImage }: ProductImagesProps) => {
   return (
-    <div className="flex md:flex-col gap-4 overflow-x-auto md:overflow-x-visible">
+    <div className="flex md:flex-row gap-8 overflow-x-auto md:overflow-x-visible">
       {images.map((image, index) => (
-        <Image
+        <div
           key={index}
-          src={image}
-          alt={`product-thumbnail-${index}`}
-          className="w-full h-full object-cover rounded-lg"
-          width={90}
-          height={90}
-          preview={false} // Disable Ant Design preview for small thumbnails
-          onClick={() => onImageSelect(image)} // Call the handler when an image is clicked
-        />
+          className={`relative ${selectedImage === image ? 'border border-black' : ''}`}
+        >
+          <Image
+            src={image}
+            alt={`product-thumbnail-${index}`}
+            className="w-full h-full object-cover rounded-lg shadow-md cursor-pointer transition-all duration-300 ease-in-out"
+            width={100}
+            height={100}
+            preview={false} // Disable Ant Design preview for small thumbnails
+            onClick={() => onImageSelect(image)} // Gọi hàm khi click vào ảnh
+          />
+          {/* Hiển thị viền hoặc hiệu ứng active cho ảnh đã chọn */}
+        </div>
       ))}
     </div>
   );
